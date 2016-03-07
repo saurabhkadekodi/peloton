175d3dca97b3d37171bb9f0539fe9e77dd388071
lendlice <chenren.xu@gmail.com>
2016-03-07 15:39:03 -0500

    Turning scanallkeys into a simple function

--- src/backend/index/bwtree.cpp
+++ src/backend/index/bwtree.cpp
@@ -2641,67 +2641,26 @@ BWTree<KeyType, ValueType, KeyComparator, KeyEqualityChecker>::ScanAllKeys(
   while (!reached_end) {
     Node<KeyType, ValueType, KeyComparator, KeyEqualityChecker>* node_pointer =
         this->table.Get(leaf_id);
-    while (node_pointer->next) {
-      switch (node_pointer->type) {
-        case (INSERT): {
-          /*DeltaNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*
-              simple_pointer =
-                  dynamic_cast<DeltaNode<KeyType, ValueType, KeyComparator,
-                                         KeyEqualityChecker>*>(node_pointer);
-          ItemPointer location = simple_pointer->value;
-          result.push_back(location);*/
-          break;
-        }
-        case (DELETE):
-          break;
-        case (SPLIT):
-          break;
-        case (MERGE):
-          break;
-        case (REMOVE):
-          break;
-        case (LEAF_BW_NODE):
-          break;
-          //{
-          /*printf("LEAF ID = %lu\n", leaf_id);
-          LeafBWNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*
-              leaf_pointer = nullptr;
-          leaf_pointer =
-              dynamic_cast<LeafBWNode<KeyType, ValueType, KeyComparator,
-                                      KeyEqualityChecker>*>(node_pointer);
-          typename multimap<KeyType, ValueType>::iterator iter =
-              leaf_pointer->kv_list.begin();
-          for (; iter != leaf_pointer->kv_list.end(); iter++) {
-            ItemPointer location = iter->second;
-            result.push_back(location);
-          }
-          if (leaf_pointer->right_sibling)
-            leaf_id = leaf_pointer->right_sibling;
-          else
-            reached_end = true;
-        }*/
-        //break;
-      }
-      node_pointer = node_pointer->next;
-    }
+    vector<ItemPointer> partial_result = this -> ScanNode(leaf_id);
+    result.insert(result.end(), partial_result.begin(), partial_result.end());
     printf("LEAF ID = %lu | size of result = %lu\n", leaf_id, result.size());
-    /*
-     * Split leaf node is not truncated; so we call Get_size on the leaf node and only iterate
-     * through those values starting from the left.
-     */
-    //FIXME: may need to change Get_size approach if we don't consolidate before ScanAllKeys.
-    LeafBWNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*
-        leaf_pointer = nullptr;
-    leaf_pointer = dynamic_cast<
-        LeafBWNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*>(
-        node_pointer);
-    typename multimap<KeyType, ValueType>::iterator iter =
-        leaf_pointer->kv_list.begin();
-    for(uint64_t i=0; i<this->Get_size(leaf_id); i++) {
-      ItemPointer location = iter->second;
-      iter++;
-      result.push_back(location);
-    }
+    // /*
+    //  * Split leaf node is not truncated; so we call Get_size on the leaf node and only iterate
+    //  * through those values starting from the left.
+    //  */
+    // //FIXME: may need to change Get_size approach if we don't consolidate before ScanAllKeys.
+    // LeafBWNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*
+    //     leaf_pointer = nullptr;
+    // leaf_pointer = dynamic_cast<
+    //     LeafBWNode<KeyType, ValueType, KeyComparator, KeyEqualityChecker>*>(
+    //     node_pointer);
+    // typename multimap<KeyType, ValueType>::iterator iter =
+    //     leaf_pointer->kv_list.begin();
+    // for(uint64_t i=0; i<this->Get_size(leaf_id); i++) {
+    //   ItemPointer location = iter->second;
+    //   iter++;
+    //   result.push_back(location);
+    // }
     printf("Siblings: left = %lu, right = %lu\n", leaf_pointer->left_sibling, leaf_pointer->right_sibling);
     if (leaf_pointer->right_sibling)
       leaf_id = leaf_pointer->right_sibling;
